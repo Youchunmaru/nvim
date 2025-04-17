7e3a5bd1c47104a19d4d01584e92dfb9e8cca802
Samuel Gröner <samuel.groener@mercatis.com>
2025-04-17 14:15:51 +0200

    add lsp configs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -219,9 +219,9 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
-
+        java_language_server = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
